c7b1235aa6a19bab4ed1b7c69dfa6ce0ecbd754a
JIaliang Han <hanjiali@umich.edu>
2020-11-24 19:46:03 +0800

    Update Hybrid_A_star.py
    cost function revised. add penalty for turning, change of direction and reverse
    
    still, some bugs

--- code/Hybrid_A_star.py
+++ code/Hybrid_A_star.py
@@ -11,9 +11,9 @@ class State:
         self.theta = theta
         self.xd = x
         self.yd = y
-        self.thetad = theta
+        self.thetad = 0#change of angle
         self.h = 0
-        self.g = 10000
+        self.g = 0
         self.f = 0
         self.parent = None
         self.children = []
@@ -22,11 +22,12 @@ class State:
         self.isgoal = False
         self.speed = 0.8
         self.deltatheta = 0.6
+        self.get_f()
 
     def cost(self, current):
         if self.isobstacle == True or current.isobstacle == True:
             return 10000
-        elif abs(math.atan2(self.yd - current.yd, self.xd - current.yd) - current.thetad) < math.pi/2:  # drive
+        elif abs(math.atan2(self.yd - current.yd, self.xd - current.xd) - current.thetad) < math.pi/2:  # drive
             return 1 * self.arclength(current) + 1.5 * self.deltaangle( current)
         else:  # reverse
             return 2 * self.arclength(current) + 1.5 * self.deltaangle(current)
@@ -52,24 +53,34 @@ class State:
             for g in gear:
                 if direction == 'left':
                     d = 1
+                    w2 = 1.05# penalty coefficient for turning
                 elif direction == 'right':
                     d = -1
+                    w2 = 1.05# penalty coefficient for turning
                 else:
                     d = 0
                 if g == 'drive':
                     w = 1
+                    w1 = 1
                 else:
                     w = -1
-                a = State(self.x, self.y, self.theta)
-                a.thetad = self.theta + d * self.deltatheta
-                tempx = self.x + w * self.speed * math.cos(a.thetad)
-                tempy = self.y + w * self.speed * math.sin(a.thetad)
+                    w1 = 2# penalty coefficent for reverse
+                a = State(self.xd, self.yd, self.thetad)
+                a.thetad = d * self.deltatheta# change of angle, positive for left turn, negative for right turn
+                if a.thetad * self.thetad < 0:
+                    w3 = 2## penalty coefficent for change of direction
+                else:
+                    w3 = 1
+                a.theta = self.theta + a.thetad
+                tempx = self.xd + w * self.speed * math.cos(a.theta)
+                tempy = self.yd + w * self.speed * math.sin(a.theta)
                 if 0 <= tempx <= max_x and 0 <= tempy <= max_y:
                     a.xd = tempx
                     a.yd = tempy
-                    a.g = self.g + self.cost(a)
+                    a.g = self.g + w1 * w2 * w3 *self.cost(a)
                     a.heuristic(goal)
                     a.roundstate()
+                    a.get_f()
                     if a.x == goal.x and a.y == goal.y:
                         a.isgoal = True
                     a.parent = self
@@ -78,6 +89,7 @@ class State:
     def roundstate(self):
         self.x = math.floor(self.xd)
         self.y = math.floor(self.yd)
+        self.theta = math.floor(self.thetad)
 
 
 class Astar:
@@ -89,6 +101,7 @@ class Astar:
     def run(self, start, goal):
         self.openlist.add(start)
         start.heuristic(goal)
+        start.get_f()
         while True:
             current = self.min_state()
             if current == -1:
@@ -103,14 +116,22 @@ class Astar:
                 for child in current.children:
                     if child.x == current.x and child.y == current.y:  # if child and current are in same cell
                         g = current.g + child.cost(current)
-                        tiebreaker = 0.1
+                        tiebreaker = 2.5
                         if child.f > current.f + tiebreaker:
                             continue
                         if (not self.exist(child, self.openlist)) or g < child.g:
                             child.parent = current
                             child.g = g
                             child.heuristic(goal)
-                            if not self.exist(child, self.openlist):
+                            child.get_f()
+                            if self.exist(child, self.openlist):
+                                for n in self.openlist:
+                                    if n.x == child.x and n.y == child.y:
+                                        self.openlist.remove(n)
+                                        self.closelist.add(n)
+                                        self.openlist.add(child)
+                                        break
+                            else:
                                 self.openlist.add(child)
                     elif not self.exist(child, self.closelist):
                         g = current.g + child.cost(current)
@@ -118,6 +139,7 @@ class Astar:
                             child.parent = current
                             child.g = g
                             child.heuristic(goal)
+                            child.get_f()
                             if not self.exist(child, self.openlist):
                                 self.openlist.add(child)
 
@@ -137,7 +159,7 @@ class Astar:
         if not self.openlist:
             return -1
         else:
-            return min(self.openlist, key=lambda x: x.g + x.h)
+            return min(self.openlist, key=lambda x: x.f)
 
     def exist(self, current, list):
         for n in list:
@@ -151,6 +173,7 @@ state = [[State(j, i, 0) for i in range(max_x)] for j in range(max_y)]
 plt.title("A star")
 state[1][3].isstart = True  # set start point
 state[5][3].isgoal = True  # set goal point
+state[5][3].theta = math.pi/2
 S = state[1][3]
 G = state[5][3]
 obstaclelist = [[3, 3], [4, 3]]
