26d7ce489f971e83cf9d10a1eada449f38c45db7
JIaliang Han <hanjiali@umich.edu>
2020-11-25 23:56:44 +0800

    Update Hybrid_A_star.py
    A star added to code for unconstrained heuristics.
    
    still some bugs.

--- code/Hybrid_A_star.py
+++ code/Hybrid_A_star.py
@@ -15,6 +15,9 @@ class State:
         self.h = 0
         self.g = 0
         self.f = 0
+        self.astar_g = 0
+        self.astar_h = 0
+        self.astar_f = 0
         self.parent = None
         self.children = []
         self.isobstacle = False
@@ -39,10 +42,18 @@ class State:
         return abs(self.thetad - current.thetad)
 
     def heuristic(self, goal):
+        # def unconh(self, goal):
+        #
+        # def conh(self, goal):
+        #
+        # self.h = max(unconh(), conh())
         self.h = self.arclength(goal) + self.deltaangle(goal)
 
     def get_f(self):
         self.f = self.g + self.h
+
+    def get_f_astar(self):
+        self.astar_f = self.astar_g + self.astar_h
 
     def successor(self, goal):
         steering = ['left', 'straight', 'right']
@@ -89,18 +100,31 @@ class State:
     def roundstate(self):
         self.x = math.floor(self.xd)
         self.y = math.floor(self.yd)
-        self.theta = math.floor(self.thetad)
+        #self.theta = math.floor(self.thetad)
 
+    def cost(self, current):
+        return math.sqrt((self.x - current.x) ** 2 + (self.y - current.y) ** 2)
 
-class Astar:
+    def astarsuccessor(self):
+        for i in range(- 1, 2):
+            for j in range(- 1, 2):
+                if (self.x + i in range(0, max_y)) and (self.y + j in range(0, max_x)):
+                    if self.x + i == self.x and self.y + j == self.y:
+                        continue
+                    if not state[self.x + i][self.y + j].isobstacle:
+                        self.children.append(state[self.x + i][self.y + j])
+
+class HAstar:
     def __init__(self):
         self.openlist = set()
         self.closelist = set()
+        self.openlist_astar = set()
+        self.closelist_astar = set()
         self.path = []
 
     def run(self, start, goal):
         self.openlist.add(start)
-        start.heuristic(goal)
+        start.h = self.unconstrained_h(start,goal)
         start.get_f()
         while True:
             current = self.min_state()
@@ -114,7 +138,8 @@ class Astar:
                 break
             else:
                 for child in current.children:
-                    if child.x == current.x and child.y == current.y:  # if child and current are in same cell
+                    if child.x == current.x and child.y == current.y:
+                        # if child and current are in same cell
                         g = current.g + child.cost(current)
                         tiebreaker = 2.5
                         if child.f > current.f + tiebreaker:
@@ -122,7 +147,7 @@ class Astar:
                         if (not self.exist(child, self.openlist)) or g < child.g:
                             child.parent = current
                             child.g = g
-                            child.heuristic(goal)
+                            start.h = self.unconstrained_h(start,goal)
                             child.get_f()
                             if self.exist(child, self.openlist):
                                 for n in self.openlist:
@@ -138,12 +163,17 @@ class Astar:
                         if (not self.exist(child, self.openlist)) or g < child.g:
                             child.parent = current
                             child.g = g
-                            child.heuristic(goal)
+                            start.h = self.unconstrained_h(start,goal)
                             child.get_f()
                             if not self.exist(child, self.openlist):
                                 self.openlist.add(child)
 
         self.get_backpointer_list(goal, start)
+
+    def unconstrained_h(self,current,goal):
+        self.astarrun(goal,current)
+        offset = math.sqrt((current.xd - current.x)^2 + (current.yd-current.y)^2)
+        return current.astar_g + offset
 
     def get_backpointer_list(self, current, start):
         self.path = [current]
@@ -161,34 +191,78 @@ class Astar:
         else:
             return min(self.openlist, key=lambda x: x.f)
 
+    def min_state_astar(self):
+        if not self.openlist_astar:
+            return -1
+        else:
+            return min(self.openlist_astar, key=lambda x: x.astar_f)
+
     def exist(self, current, list):
         for n in list:
             if n.x == current.x and n.y == current.y and n.theta == current.theta:
                 return True
 
 
+
+
+
+    def astarexist(current, list):
+        for n in list:
+            if n.x == current.x and n.y == current.y:
+                return True
+
+    def astarrun(self, start, goal):
+        self.openlist_astar.add(start)
+        start.astar_h = start.cost( goal)
+        start.get_f_astar()
+        while True:
+            current = self.min_state_astar()
+            current.astarsuccessor()
+            self.openlist_astar.remove(current)
+            self.closelist_astar.add(current)
+
+            if current.isgoal:
+                break
+            else:
+                for child in current.children:
+                    if self.astarexist(child, self.closelist_astar):
+                        continue
+                    if not self.astarexist(child, self.openlist_astar):
+                        self.openlist_astar.add(child)
+                        child.parent = current
+                        child.astar_g = current.astar_g + child.cost( current)
+                        child.astar_h = child.cost( goal)
+                        child.get_f_astar()
+                    elif current.astar_g + child.cost( current) < child.astar_g:
+                        child.parent = current
+                        child.astar_g = current.astar_g + child.cost(current)
+                        child.astar_h = child.cost( goal)
+                        child.get_f_astar()
+
+
 max_x = 7
 max_y = 6
 state = [[State(j, i, 0) for i in range(max_x)] for j in range(max_y)]
-plt.title("A star")
+plt.title("HA star")
 state[1][3].isstart = True  # set start point
 state[5][3].isgoal = True  # set goal point
 state[5][3].theta = math.pi/2
 S = state[1][3]
 G = state[5][3]
 obstaclelist = [[3, 3], [4, 3]]
+for n in obstaclelist:
+    state[n[0]][n[1]].isobstacle = True
+
 startTime = time.time()
-astar = Astar()
-astar.run(S, G)
+hastar = HAstar()
+hastar.run(S, G)
+
 x = []
 y = []
-for n in astar.path:
+for n in hastar.path:
     x.append(n.xd)
     y.append(n.yd)
 print("It took %s seconds to run" % (time.time() - startTime))
-
-for n in obstaclelist:
-    state[n[0]][n[1]].isobstacle = True
 
 plt.xlim((0, max_x))
 plt.ylim((0, max_y))
